9967001cc7669638cefa5a847f1ed0dea186efce
lovedr6s <praense@icloud.com>
2024-09-12 10:22:07 +0300

    Added progression

---
+++ brain_games/games/progression.py
@@ -0,0 +1,15 @@
+import random
+
+DESCRIPTION = "What number is missing in the progression?"
+
+def play():
+    first_number = random.randint(0,10)
+    working_number = random.randint(0,10)
+    result = []
+    choosen_number = random.randint(0, 9)
+    for _ in range (0, 10):
+        result.append(first_number)
+        first_number += working_number
+    correct = result[choosen_number]
+    result[choosen_number] = ".."
+    return result, str(correct)
