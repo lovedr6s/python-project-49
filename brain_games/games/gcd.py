2ba42e876ce7118c8815842a8f3f403557cf4403
lovedr6s <praense@icloud.com>
2024-09-12 11:38:16 +0300

    Made gcd code better

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,28 +2,15 @@ import random
 
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
 def play():
-    one_numbers = []
-    second_numbers = []
-    num_one = random.randint(0, 100)
-    num_two = random.randint(0, 100)
-
-    for num in range(1, num_one + 1):
-        if num_one % num == 0:
-            one_numbers.append(num)
-
-
-    for num_second in range(1, num_two + 1):
-        if num_two % num_second == 0:
-            second_numbers.append(num_second)
-
-
-    for count in one_numbers:
-        for count_second in second_numbers:
-            if count == count_second:
-                correct = count 
-
-
-    return f"{num_one} {num_two}", str(correct)
-
+    num_one = 5#random.randint(1, 10)
+    num_two = 10#random.randint(1, 10)
     
+    correct = gcd(num_one, num_two)
+    
+    return f"{num_one} {num_two}", str(correct)
