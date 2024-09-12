edf47a1d032c43cb92ebdb5b5cb50c18b70fa217
lovedr6s <praense@icloud.com>
2024-09-12 12:33:04 +0300

    Added new string to even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,7 +1,7 @@
 import random
 
 
-DESCRIPTION = "\"yes\" if the number is even, otherwise answer \"no\"."
+DESCRIPTION = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
 
 def play():
