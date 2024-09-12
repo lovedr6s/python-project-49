ee4ae8b06c8e50575ff529eae8e720ed28d3f613
lovedr6s <praense@icloud.com>
2024-09-12 13:23:10 +0300

    Removed first number from prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,7 +3,7 @@ import random
 DESCRIPTION = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
 
 def play():
-    first_number = random.randint(1,10)
+    first_number = random.randint(2,10)
     correct = "yes"
     for char in range(2, first_number):
         if first_number % char == 0:
