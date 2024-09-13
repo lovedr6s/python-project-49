cf2072bd063256c2af69445dc6cb2dd3ca8df76c
lovedr6s <praense@icloud.com>
2024-09-13 10:53:18 +0300

    added var name to the loosing text

--- brain_games/brain_main.py
+++ brain_games/brain_main.py
@@ -15,7 +15,7 @@ def main(module_game):
             print("Correct!")
         else:
             print(f"'{answer}' is wrong answer ;(. "
-                  "Correct answer was '{correct}')")
+                  f"Correct answer was '{correct}')")
             print(f"Let's try again, {name}!")
             won = False
             break
