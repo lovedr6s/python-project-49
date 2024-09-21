113a320abcd5611bc6dbb04ff85673462d806617
lovedr6s <praense@icloud.com>
2024-09-21 13:01:39 +0300

    agan

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games.cli import welcome_user
 
 
 def main():
-    cli.welcome_user()
+    welcome_user()
 
 
 if __name__ == "__main__":
